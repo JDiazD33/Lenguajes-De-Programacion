32f5216bfc2d1629c12454c2ffccaa894cd130ae
JDiazD33 <U23232498@utp.edu.pe>
2026-06-29 13:11:57 -0500

    fix: configure host for production deploy

--- main.py
+++ main.py
@@ -28,4 +28,5 @@ if __name__ == "__main__":
     print("Servidor iniciado en: http://127.0.0.1:5000")
     print("Presione CTRL+C para detener.")
     print("=" * 60)
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=False)
+
